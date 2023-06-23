31fb4dc07037cf29aafeb74da38756bc66081f6e
zifrit <zifrit@list.ru>
2023-06-23 16:17:55 +0300

    fixed

--- task_management/task_management/wsgi.py
+++ task_management/task_management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_managemnt project.
+WSGI config for task_management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_managemnt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_management.settings')
 
 application = get_wsgi_application()
